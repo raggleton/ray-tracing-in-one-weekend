bf7ff44d5ac27d847b8d7ba6590981127c76a915
Robin Aggleton <RobinAggleton@gmail.com>
2022-12-28 12:01:41 +0000

    End of Ch6 - two hittable spheres

--- main.cc
+++ main.cc
@@ -1,24 +1,32 @@
 #include <iostream>
 
+#include "rtWeekend.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
 
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
-    // determine if ray `r` intersects sphere centeres at `center` with radius `radius`
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+    // determine t when ray `r` intersects sphere centeres at `center` with radius `radius`
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
-color ray_color(const ray& r) {
-    // if intersects sphere, color red
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+color ray_color(const ray& r, const hittable& world) {
+    // if intersects any hittable object in our `world`, set color using unit vector
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec))
+    {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
     // otherwise, color using background gradient
     vec3 unit_direction = unit_vector(r.direction());
     // t is a parameter that goes from 0 to 1 as y goes from -1 to 1
@@ -34,6 +42,11 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5)); // normal sphere
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100)); // "ground"
 
     // Camera
     auto viewport_height = 2.0;
@@ -55,7 +68,7 @@ int main() {
             auto u = double(i) / (image_width-1);
             auto v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
